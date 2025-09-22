534d18425793750bde2a47c49eb10fc3900e768f
Yoshihide Shimoji <g.y.shimoji@gmail.com>
2025-09-22 23:14:25 +0900

    zsh機能を追加

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -3,3 +3,6 @@ export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PA
 
 # Homebrew Git を優先
 export PATH="/opt/homebrew/bin:$PATH"
+
+# $ZDOTDIR/scripts/
+export PATH="/Users/yoshihide/dotfiles-mac/scripts:$PATH"
