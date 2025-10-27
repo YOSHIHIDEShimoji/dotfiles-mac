284544e7809e2cde4d4d79a1ff81fabeadb602f4
Yoshihide Shimoji <g.y.shimoji@gmail.com>
2025-10-27 11:17:16 +0900

    python3をbrewをデフォルトにするように設定

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -1,14 +1,18 @@
+# Homebrew python & git を最優先
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+
 # dotfiles repo root
 export DOTFILES="$HOME/dotfiles-mac"
+
+# $ZDOTDIR/scripts/
+export PATH="$HOME/dotfiles-mac/scripts:$PATH"
+
+# tex path
+export PATH="/Library/TeX/texbin:$PATH"
 
 # VSCode CLI
 export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
 
-# Homebrew Git を優先
-export PATH="/opt/homebrew/bin:$PATH"
+# PATH の重複を除去（zsh専用）
+typeset -U path PATH
 
-# $ZDOTDIR/scripts/
-export PATH="/Users/yoshihide/dotfiles-mac/scripts:$PATH"
-
-# tex path
-export PATH="/Library/TeX/texbin:$PATH"
