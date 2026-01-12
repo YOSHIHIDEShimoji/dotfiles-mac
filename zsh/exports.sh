eea6b03f8400551277a08f0f6034fb10cc5229d6
Yoshihide Shimoji <g.y.shimoji@gmail.com>
2026-01-13 00:53:23 +0900

    modified: zsh/exports.sh

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -1,23 +1,20 @@
-# Homebrew python & git を最優先
-export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
-
-# dotfiles repo root
-export DOTFILES="$HOME/dotfiles-mac"
-
-# $ZDOTDIR/scripts/
-export PATH="$HOME/dotfiles-mac/scripts:$PATH"
-
-# tex path
-export PATH="/Library/TeX/texbin:$PATH"
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
 # VSCode CLI
 export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
 
-# PATH の重複を除去（zsh専用）
+# tex path
+export PATH="/Library/TeX/texbin:$PATH"
+
+# dotfiles scripts
+export PATH="$HOME/dotfiles-mac/scripts:$PATH"
+
+# Homebrew (最優先にしたいので、下の方に書くか、最後に結合する)
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+
+# 重複除去
 typeset -U path PATH
 
-# sem を設定
-export SEM="$HOME/Library/CloudStorage/GoogleDrive-g.y.shimoji@gmail.com/マイドライブ/Chiba-u(drive)/2nd/sem2"
-
-# sbin
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+# 変数設定
+export DOTFILES="$HOME/dotfiles-mac"
+export SEM="$HOME/Library/CloudStorage/GoogleDrive-g.y.shimoji@gmail.com/マイドライブ/Chiba-u(drive)/2nd/sem2"
