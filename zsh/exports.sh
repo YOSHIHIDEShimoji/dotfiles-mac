1c875b8e2f38653da33db2112fe825375e78822a
Yoshihide Shimoji <g.y.shimoji@gmail.com>
2026-01-31 09:47:58 +0900

    create new scripts: zsh/plubins/pyenv.sh to activate pyenv

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -1,3 +1,4 @@
+# macOS
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
 # VSCode CLI
@@ -9,6 +10,9 @@ export PATH="/Library/TeX/texbin:$PATH"
 # Homebrew
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
 
+# Java
+export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"
+
 # dotfiles-mac/scripts 配下のサブディレクトリ以下全てに PATH を通し、実行権限を与える。
 if [ -d "$HOME/dotfiles-mac/scripts" ]; then
     export PATH="$PATH:$HOME/dotfiles-mac/scripts"
@@ -16,7 +20,6 @@ if [ -d "$HOME/dotfiles-mac/scripts" ]; then
         export PATH="$PATH:$dir"
     done
 fi
-
 chmod -R +x ~/dotfiles-mac/scripts/
 
 # 重複除去
@@ -24,4 +27,4 @@ typeset -U path PATH
 
 # 変数設定
 export DOTFILES="$HOME/dotfiles-mac"
-export SEM="$HOME/Library/CloudStorage/GoogleDrive-g.y.shimoji@gmail.com/マイドライブ/Chiba-u(drive)/2nd/sem2"
+export SEM="$HOME/Library/CloudStorage/GoogleDrive-g.y.shimoji@gmail.com/マイドライブ/chiba-u-drive/2nd/sem2"
---
+++ zsh/plugins/pyenv.zsh
@@ -0,0 +1,4 @@
+# pyenv有効化
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
