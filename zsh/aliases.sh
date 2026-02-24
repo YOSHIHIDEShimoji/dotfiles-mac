16ed3c593fba5a068706dc330081106a35c89fd0
Yoshihide Shimoji <g.y.shimoji@gmail.com>
2026-02-24 15:34:22 +0900

    エイリアスの短縮・追加・整理（fd/dust/rg/procs対応、cd階層追加、port修正）

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -6,25 +6,29 @@ alias lt='eza --tree'
 alias grep='grep --color=auto'
 
 # find
-alias fd='fd --hidden --exclude .git' # 隠しファイルも含めるが .git は除外
+alias f='fd'
+alias fh='fd --hidden --exclude .git --exclude node_modules --exclude .venv --exclude .cache'
 
 # dust
-alias du='dust -r'
-alias df='dust -r -d 1'
+alias d='dust -r'
+
+# rg
+alias r='rg'
+
+# procs
+alias p='procs'
 
 # ターミナル
 alias h='history'
 alias ..='cd ..'
 alias ...='cd ../..'
+alias ....='cd ../../..'
 alias mkdir='mkdir -p'
 
 # 確認
 alias rm='trash -v'
 alias mv='mv -i'
-alias cp='cp -Ri'
-
-# clear 一発できれいにする
-alias clear='clear; clear'
+alias cp='cp -i'
 
 # git
 alias g='git'
@@ -44,7 +48,7 @@ alias gpl='git pull'
 
 # ネットワーク・システム
 alias myip='curl ifconfig.me'
-alias port='lsof -i -P'
+alias port='lsof -i -P -n'
 
 # リンク
 alias moodle='open -na "Google Chrome" --args --profile-directory="Profile 1" "https://moodle.gs.chiba-u.jp/moodle/my/"'
